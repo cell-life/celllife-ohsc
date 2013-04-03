5b506433b48bd30729b93cfa261beace069a6c04
Kevin Sewell <kevin.sewell@aronim.com>
2013-04-03 11:56:24 +0200

    Adding default ehcache.xml

--- webapp/src/test/java/org/celllife/ohsc/integration/cqm/CqmUssdSubmissionMediatorAcceptanceTest.java
+++ webapp/src/test/java/org/celllife/ohsc/integration/cqm/CqmUssdSubmissionMediatorAcceptanceTest.java
@@ -1,13 +1,17 @@
 package org.celllife.ohsc.integration.cqm;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.http.Header;
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.protocol.HttpContext;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
@@ -23,7 +27,7 @@ public class CqmUssdSubmissionMediatorAcceptanceTest {
     public static final String BASE_DIR = "/data/CqmUssdSubmissionRequest/";
 
     @Test
-//    @Ignore("Requires running Jetty instance")
+    @Ignore("Used for generating test data")
     public void testHandleCqmUssdSubmissionLoad() throws Exception {
 
         String path = getClass().getResource(BASE_DIR).getPath();
@@ -43,6 +47,9 @@ public class CqmUssdSubmissionMediatorAcceptanceTest {
         HttpConnectionParams.setSoTimeout(params, 10000);
 
         method.setEntity(new StringEntity(json));
+        UsernamePasswordCredentials credentials = new UsernamePasswordCredentials("user@test.cell-life.org", "P@ssw0rd1");
+        Header authenticate = BasicScheme.authenticate(credentials, "US-ASCII", false);
+        method.addHeader(authenticate);
         method.addHeader(new BasicHeader("Content-Type", "application/json"));
 
         DefaultHttpClient client = new DefaultHttpClient();
